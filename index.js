6f08a35f15ef7bac03297ec8a0a900400f34022d
mtrkali <tarakfeni95@gmail.com>
2025-08-18 07:07:27 +0600

    added user api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -19,42 +19,69 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
     // Send a ping to confirm a successful connection
     const tipsCollection = client.db("gardenDB").collection("tips");
+    const gardenersCollection = client.db("gardenDB").collection("gardenaers");
+    const usersCollection = client.db("gardenDB").collection("users");
 
     // tips post -1
-    app.post('/tips', async(req, res)=>{
+    app.post("/tips", async (req, res) => {
       const newTip = req.body;
       const result = await tipsCollection.insertOne(newTip);
       res.send(result);
-    })
+    });
 
     //tips get -2
-    app.get('/tips', async(req, res) => {
-      const result = await tipsCollection.find().toArray();
+    app.get("/tips", async (req, res) => {
+      const availability = req.query.availability;
+      const query = { availability: "public" };
+      const result = await tipsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //tips get single ids element -3
+    app.get("/tips/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tipsCollection.findOne(query);
+      res.send(result);
+    });
+
+    //gardeners get -1
+    app.get("/gardenaers", async (req, res) => {
+      const result = await gardenersCollection.find().toArray();
+      res.send(result);
+    });
+
+    //users post -1
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+    
+    //user get -2
+    app.get('/users', async(req, res) =>{
+      const result = await usersCollection.find().toArray();
       res.send(result);
     })
 
-    app.get("/", (req, res) => {
-      res.send("Our garden server will be comming soon");
-    });
-
-    app.listen(port, () => {
-      console.log("our server running on port ", port);
-    });
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Our garden server will be comming soon");
+});
+
+app.listen(port, () => {
+  console.log("our server running on port ", port);
+});
